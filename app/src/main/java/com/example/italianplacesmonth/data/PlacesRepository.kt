f7f46914b71811237911be2e424684295a28d060
PaoloCaldera <paolo.caldera@outlook.com>
2024-01-19 15:39:35 +0100

    feature: build the repository layer of the app
    In the repository layer of the app, define an object PlacesRepository that contains a method for loading all the places. Use the method for passing the list of places to be displayed.
    
    Integrates #3

---
+++ app/src/main/java/com/example/italianplacesmonth/data/PlacesRepository.kt
@@ -0,0 +1,217 @@
+package com.example.italianplacesmonth.data
+
+import com.example.italianplacesmonth.R
+import com.example.italianplacesmonth.model.Place
+
+object PlacesRepository {
+    fun loadPlaces(): List<Place> =
+        listOf(
+            Place(
+                title = R.string.title_1,
+                city = R.string.city_1,
+                region = R.string.region_1,
+                year = R.string.year_1,
+                image = R.drawable.image_1
+            ),
+            Place(
+                title = R.string.title_2,
+                city = R.string.city_2,
+                region = R.string.region_2,
+                year = R.string.year_2,
+                image = R.drawable.image_2
+            ),
+            Place(
+                title = R.string.title_3,
+                city = R.string.city_3,
+                region = R.string.region_3,
+                year = R.string.year_3,
+                image = R.drawable.image_3
+            ),
+            Place(
+                title = R.string.title_4,
+                city = R.string.city_4,
+                region = R.string.region_4,
+                year = R.string.year_4,
+                image = R.drawable.image_4
+            ),
+            Place(
+                title = R.string.title_5,
+                city = R.string.city_5,
+                region = R.string.region_5,
+                year = R.string.year_5,
+                image = R.drawable.image_5
+            ),
+            Place(
+                title = R.string.title_6,
+                city = R.string.city_6,
+                region = R.string.region_6,
+                year = R.string.year_6,
+                image = R.drawable.image_6
+            ),
+            Place(
+                title = R.string.title_7,
+                city = R.string.city_7,
+                region = R.string.region_7,
+                year = R.string.year_7,
+                image = R.drawable.image_7
+            ),
+            Place(
+                title = R.string.title_8,
+                city = R.string.city_8,
+                region = R.string.region_8,
+                year = R.string.year_8,
+                image = R.drawable.image_8
+            ),
+            Place(
+                title = R.string.title_9,
+                city = R.string.city_9,
+                region = R.string.region_9,
+                year = R.string.year_9,
+                image = R.drawable.image_9
+            ),
+            Place(
+                title = R.string.title_10,
+                city = R.string.city_10,
+                region = R.string.region_10,
+                year = R.string.year_10,
+                image = R.drawable.image_10
+            ),
+            Place(
+                title = R.string.title_11,
+                city = R.string.city_11,
+                region = R.string.region_11,
+                year = R.string.year_11,
+                image = R.drawable.image_11
+            ),Place(
+                title = R.string.title_12,
+                city = R.string.city_12,
+                region = R.string.region_12,
+                year = R.string.year_12,
+                image = R.drawable.image_12
+            ),
+            Place(
+                title = R.string.title_13,
+                city = R.string.city_13,
+                region = R.string.region_13,
+                year = R.string.year_13,
+                image = R.drawable.image_13
+            ),Place(
+                title = R.string.title_14,
+                city = R.string.city_14,
+                region = R.string.region_14,
+                year = R.string.year_14,
+                image = R.drawable.image_14
+            ),Place(
+                title = R.string.title_15,
+                city = R.string.city_15,
+                region = R.string.region_15,
+                year = R.string.year_15,
+                image = R.drawable.image_15
+            ),
+            Place(
+                title = R.string.title_16,
+                city = R.string.city_16,
+                region = R.string.region_16,
+                year = R.string.year_16,
+                image = R.drawable.image_16
+            ),
+            Place(
+                title = R.string.title_17,
+                city = R.string.city_17,
+                region = R.string.region_17,
+                year = R.string.year_17,
+                image = R.drawable.image_17
+            ),
+            Place(
+                title = R.string.title_18,
+                city = R.string.city_18,
+                region = R.string.region_18,
+                year = R.string.year_18,
+                image = R.drawable.image_18
+            ),
+            Place(
+                title = R.string.title_19,
+                city = R.string.city_19,
+                region = R.string.region_19,
+                year = R.string.year_19,
+                image = R.drawable.image_19
+            ),
+            Place(
+                title = R.string.title_20,
+                city = R.string.city_20,
+                region = R.string.region_20,
+                year = R.string.year_20,
+                image = R.drawable.image_20
+            ),
+            Place(
+                title = R.string.title_21,
+                city = R.string.city_21,
+                region = R.string.region_21,
+                year = R.string.year_21,
+                image = R.drawable.image_21
+            ),
+            Place(
+                title = R.string.title_22,
+                city = R.string.city_22,
+                region = R.string.region_22,
+                year = R.string.year_22,
+                image = R.drawable.image_22
+            ),
+            Place(
+                title = R.string.title_23,
+                city = R.string.city_23,
+                region = R.string.region_23,
+                year = R.string.year_23,
+                image = R.drawable.image_23
+            ),
+            Place(
+                title = R.string.title_24,
+                city = R.string.city_24,
+                region = R.string.region_24,
+                year = R.string.year_24,
+                image = R.drawable.image_24
+            ),
+            Place(
+                title = R.string.title_25,
+                city = R.string.city_25,
+                region = R.string.region_25,
+                year = R.string.year_25,
+                image = R.drawable.image_25
+            ),
+            Place(
+                title = R.string.title_26,
+                city = R.string.city_26,
+                region = R.string.region_26,
+                year = R.string.year_26,
+                image = R.drawable.image_26
+            ),
+            Place(
+                title = R.string.title_27,
+                city = R.string.city_27,
+                region = R.string.region_27,
+                year = R.string.year_27,
+                image = R.drawable.image_27
+            ),
+            Place(
+                title = R.string.title_28,
+                city = R.string.city_28,
+                region = R.string.region_28,
+                year = R.string.year_28,
+                image = R.drawable.image_28
+            ),
+            Place(
+                title = R.string.title_29,
+                city = R.string.city_29,
+                region = R.string.region_29,
+                year = R.string.year_29,
+                image = R.drawable.image_29
+            ),
+            Place(
+                title = R.string.title_30,
+                city = R.string.city_30,
+                region = R.string.region_30,
+                year = R.string.year_30,
+                image = R.drawable.image_30
+            )
+        )
+}
